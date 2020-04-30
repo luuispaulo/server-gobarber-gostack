3930f6cec0d78f7f99b4c88185a60e8b5a603200
Louzada <silvalouzada@gmail.com>
2020-04-30 13:36:23 -0300

    feat: add typeorm and pg driver to config a connection

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import app from './app';
 
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
-  console.log('🚀 Server started on port 3333!');
+  console.log('✅ 🚀 Server started on port 3333!');
 });
