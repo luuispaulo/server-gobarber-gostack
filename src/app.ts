62573594d4571ec49412ac306f30d043149586a8
Louzada <silvalouzada@gmail.com>
2020-05-11 00:14:57 -0300

    feat: add GET static file '/static/:fileName'

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ import express from 'express';
 import routes from './routes';
 import './database';
 
+import upload from './config/upload';
+
 const app = express();
 app.use(express.json());
+app.use('/static', express.static(upload.directory));
 app.use(routes);
 
 export default app;
